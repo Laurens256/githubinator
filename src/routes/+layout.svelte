40009b8bcf56ad9df6df85bf8903313e7a62d58e
Laurens256 <duinlaurens2@gmail.com>
2023-03-13 13:36:28 +0100

    server B)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,35 +2,6 @@
 	import { request, gql, GraphQLClient } from 'graphql-request';
 	import '../app.css';
 	import Header from '$lib/header.svelte';
-
-	const client = new GraphQLClient('https://api.github.com/graphql', {
-    headers: {
-      Authorization: 'Bearer <bearer>',
-    },
-  })
-
-	const query = gql`
-		{
-			viewer {
-				repositoriesContributedTo(first: 100, contributionTypes: [COMMIT, PULL_REQUEST]) {
-					nodes {
-						nameWithOwner
-					}
-				}
-				repositories(first: 100, ownerAffiliations: [OWNER]) {
-					nodes {
-						nameWithOwner
-					}
-				}
-			}
-		}
-	`;
-
-	const func = async() => {
-	const data = await client.request(query);
-	console.log(await data);
-	}
-	// func();
 </script>
 
 <Header />
---
+++ src/routes/+page.server.ts
@@ -0,0 +1,37 @@
+import { request, gql, GraphQLClient } from 'graphql-request';
+import { SECRET_GITHUB_TOKEN } from '$env/static/private';
+
+const api_uri = 'https://api.github.com/graphql';
+
+export async function load({ params }) {
+	const client = new GraphQLClient(api_uri, {
+		headers: {
+			Authorization: `Bearer ${SECRET_GITHUB_TOKEN}`,
+			'Content-Type': 'application/json'
+		}
+	});
+
+	const query = gql`
+		{
+			viewer {
+				repositoriesContributedTo(first: 100, contributionTypes: [COMMIT, PULL_REQUEST]) {
+					nodes {
+						nameWithOwner
+					}
+				}
+				repositories(first: 100, ownerAffiliations: [OWNER]) {
+					nodes {
+						nameWithOwner
+					}
+				}
+			}
+		}
+	`;
+
+	try {
+		const data = await client.request(query);
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+}
